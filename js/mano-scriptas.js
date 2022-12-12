f4b58c73499da0250204a18503ca03f1d8d2fe04
JustasMe97 <meskauskasjustas@gmail.com>
2022-12-12 15:18:22 +0200

    First commit

---
+++ js/mano-scriptas.js
@@ -0,0 +1,24 @@
+/*Pirma užuotis*/
+let a=1;
+console.log('Kintamojo a reikšmė: ' + a)
+a=2;
+console.log('Kintamojo a reikšmė: ' + a)
+
+/*Antra užduotis*/
+let b='Justas';
+let c='Mano vardas ';
+console.log(c + b);
+
+/*Trečia užduoti
+let metai=1997;
+let menuo=12;
+let diena=15;
+console.log('Data: '+metai +' '+menuo+' '+diena);
+
+let metaiPaskutinis=metai % 10;
+let menuoPaskutinis=menuo % 10;
+let dienaPaskutinis=diena % 10;
+let sumaPaskutiniu=metaiPaskutinis+menuoPaskutinis+dienaPaskutinis;
+console.log('Paskutinių datos skaičių reikšmė lygi: ' + sumaPaskutiniu)
+
+
