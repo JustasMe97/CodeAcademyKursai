ece60c0a4edfb99a7b8400d2d3e2eb2900042082
JustasMe97 <meskauskasjustas@gmail.com>
2022-12-15 11:51:41 +0200

    patobulinau užduotį

--- js/matematika2.js
+++ js/matematika2.js
@@ -3,74 +3,21 @@ let arr1=[1,2,3,4,5];
 let arr2=[6,7,8,9,10];
 
 function veiksmai(zenklas){
-    if(zenklas==='+') {
-        document.getElementById('ats').innerHTML += '<br> Sudeties funkcija: <br>';
+        document.getElementById('ats').innerHTML += '<br>' +zenklas+' funkcija: <br>';
         for (let i = 0; i < arr1.length; i++) {
-            skaicius = arr1[i] + arr2[i];
-            document.getElementById('ats').innerHTML += 'Skaiciaus ' + arr1[i]+' ir skaiciaus '+arr2[i]+' sudetis = '+skaicius+'<br>';
+            skaicius = eval(arr1[i] +zenklas+ arr2[i]);
+            document.getElementById('ats').innerHTML += 'Skaiciaus ' + arr1[i]+' ir skaiciaus '+arr2[i]+' "'+zenklas+'" veiksmas = '+skaicius+'<br>';
         }
-    }
-    if (zenklas==='-'){
-        document.getElementById('ats').innerHTML+='<br> Atimties funkcija: <br>';
-        for(let i=0; i<arr1.length; i++){
-            skaicius=arr1[i]-arr2[i];
-            document.getElementById('ats').innerHTML+='Skaiciaus ' + arr1[i]+' ir skaiciaus '+arr2[i]+' atimtis = '+skaicius+'<br>';
-        }
-    }
-    if (zenklas==='/'){
-        document.getElementById('ats').innerHTML+='<br> Dalybos funkcija: <br>';
-        for(let i=0; i<arr1.length; i++){
-            skaicius=arr1[i]/arr2[i];
-            document.getElementById('ats').innerHTML+='Skaiciaus ' + arr1[i]+' ir skaiciaus '+arr2[i]+' dalyba = '+skaicius.toFixed(2)+'<br>';
-        }
-        }
-    if (zenklas==='+'){
-        document.getElementById('ats').innerHTML+='<br> Daugybos funkcija: <br>';
-        for(let i=0; i<arr1.length; i++){
-            skaicius=arr1[i]*arr2[i];
-            document.getElementById('ats').innerHTML+='Skaiciaus ' + arr1[i]+' ir skaiciaus '+arr2[i]+' daugyba = '+skaicius+'<br>';
-        }
-    }
-
 }
 
 function veiksmaiVisiSuVisais(zenklas){
-    if(zenklas==='+') {
-        document.getElementById('ats').innerHTML += '<br> Sudeties visi su visais funkcija: <br>';
+        document.getElementById('ats').innerHTML +=  '<br>' +zenklas+' funkcija visi su visais: <br>';
         for (let i = 0; i < arr1.length; i++) {
             for (let j = 0; j < arr2.length; j++) {
-                skaicius = arr1[i] + arr2[j];
-                document.getElementById('ats').innerHTML += 'Skaiciaus ' + arr1[i] + ' ir skaiciaus ' + arr2[j] + ' sudetis = ' + skaicius + '<br>';
+                skaicius = eval(arr1[i] +zenklas+ arr2[j]);
+                document.getElementById('ats').innerHTML += 'Skaiciaus ' + arr1[i] + ' ir skaiciaus ' + arr2[j] +' "'+zenklas+'" veiksmas = ' + skaicius + '<br>';
             }
         }
-    }
-    if(zenklas==='-') {
-        document.getElementById('ats').innerHTML += '<br> Atimties visi su visais funkcija: <br>';
-        for (let i = 0; i < arr1.length; i++) {
-            for (let j = 0; j < arr2.length; j++) {
-                skaicius = arr1[i] - arr2[j];
-                document.getElementById('ats').innerHTML += 'Skaiciaus ' + arr1[i] + ' ir skaiciaus ' + arr2[j] + ' atimtis = ' + skaicius + '<br>';
-            }
-        }
-    }
-    if(zenklas==='/') {
-        document.getElementById('ats').innerHTML+='<br> Dalybos visi su visais funkcija: <br>';
-        for(let i=0; i<arr1.length; i++){
-            for(let j=0;j<arr2.length; j++){
-                skaicius=arr1[i]/arr2[j];
-                document.getElementById('ats').innerHTML+='Skaiciaus ' + arr1[i]+' ir skaiciaus '+arr2[j]+' dalyba = '+skaicius.toFixed(2)+'<br>';
-            }
-        }
-    }
-    if(zenklas==='*') {
-        document.getElementById('ats').innerHTML+='<br> Daugybos visi su visais funkcija: <br>';
-        for(let i=0; i<arr1.length; i++){
-            for(let j=0;j<arr2.length; j++){
-                skaicius=arr1[i]*arr2[j];
-                document.getElementById('ats').innerHTML+='Skaiciaus ' + arr1[i]+' ir skaiciaus '+arr2[j]+' daugyba = '+skaicius+'<br>';
-            }
-        }
-    }
 }
 
 veiksmai('+');
